1bcadf19ddf3a5b6cedb9cdbf012281d883dd9ab
Henry Fuentes <henry.fg51@gmail.com>
2026-04-22 17:23:25 -0600

    add extract_files.py

---
+++ src/etl/extract_files.py
@@ -0,0 +1,53 @@
+"""
+--------------------------------------------------------------------------------
+@company      : 
+@author       : Henry Fuentes
+@created_data : 2026-04-22
+@jira_task    : 
+@description  : por medio de este script vamos a hacer la extraccion de los datos de los archivos .csv y luego se van a cargar a la base de datos 
+                
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+import sys
+import requests
+import pandas as pd
+import boto3
+from awsglue.utils import getResolvedOptions
+from aws_utils import AWSOperations
+import awswrangler as wr
+
+
+###########################PATHS################################
+PATH_SALES = 'data-platform-lab/input/sales.csv/'
+###########################PATHS################################
+
+
+args = getResolvedOptions(sys.argv,['secret_manager', 'output_s3', 'dynamodb_table_name'])
+
+aws_ops = AWSOperations(
+    dynamo_table=args['dynamodb_table_name'],
+    secret_path=args['secret_manager'],
+    s3_bucket=args['output_s3'],
+    profile=None,
+    region="us-east-1"
+)
+
+secrets = aws_ops.get_secret()
+base_url = secrets["sports_api_base_url"]
+s3_bucket = f"{args['output_s3']}"
+
+print(base_url)
+print(s3_bucket)
+
+def extract_data_from_csv():
+    df = wr.s3.read_csv(f's3://{s3_bucket}/{PATH_SALES}')
+    return df
+
+#s3://data-platform-dev-main-570435244160/data-platform-lab/input/sales/sales.csv
+
+
+#saving data frame in a parquet file
+# s3_destination_path = f's3://{s3_bucket}/data-platform-lab/Soccer_Files/'
+# wr.s3.to_parquet(df=df_final,path=s3_destination_path,database='data_platform_dev_db',table='soccer_data',mode="overwrite_partitions",dataset=True)
--- src/etl/main_script.py
+++ src/etl/main_script.py
@@ -9,58 +9,13 @@
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
 """
-import sys
-import requests
-import pandas as pd
-import boto3
-from awsglue.utils import getResolvedOptions
-from aws_utils import AWSOperations
-import awswrangler as wr
 
+from extract_files import extract_data_from_csv
 
-args = getResolvedOptions(sys.argv,['secret_manager', 'output_s3', 'dynamodb_table_name'])
+def main():
+    print("Inicio del proceso de ETL")
+    extract_data_from_csv()
+    print("Fin del proceso de ETL")
 
-aws_ops = AWSOperations(
-    dynamo_table=args['dynamodb_table_name'],
-    secret_path=args['secret_manager'],
-    s3_bucket=args['output_s3'],
-    profile=None,
-    region="us-east-1"
-)
-
-secrets = aws_ops.get_secret()
-base_url = secrets["sports_api_base_url"]
-s3_bucket = f"{args['output_s3']}"
-
-print(base_url)
-print(s3_bucket)
-
-url = base_url
-
-response = requests.get(url)
-response.raise_for_status()
-
-data = response.json()
-df = pd.DataFrame(data["response"])
-columns = ['title', 'competition', 'date']
-
-df_final = df[columns]
-
-#saving data frame in a parquet file
-s3_destination_path = f's3://{s3_bucket}/data-platform-lab/Soccer_Files/'
-wr.s3.to_parquet(df=df_final,path=s3_destination_path,database='data_platform_dev_db',table='soccer_data',mode="overwrite_partitions",dataset=True)
-
-#function to store the dataframe in a table 
-# def write_to_s3(df, s3_bucket, path_iati, sub_path, table_name, dtype=None):
-#     s3_destination_path = f's3://{s3_bucket}/{path_iati}/{sub_path}/'
-#     wr.s3.to_parquet(
-#         df=df,
-#         path=s3_destination_path,
-#         database='iadb-dmt-db',
-#         table=table_name,
-#         mode="overwrite",
-#         dataset=True,
-#         dtype=dtype
-#         )
-
-# write_to_s3(df_final, s3_test_row, path_test, 'locations_projects_AFP', 'edw_dmt_iati_locations_projects_AFP')
+if __name__ == "__main__":
+    main()
